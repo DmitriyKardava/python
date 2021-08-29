a42e1e82b761fd0f9f7e10667cfb97ca2df1f67a
DmitriyKardava <dmitriy.kardava@gmail.com>
2021-08-29 13:13:35 +0300

    elif вместо if

--- 1_3.py
+++ 1_3.py
@@ -1,5 +1,5 @@
 for percent in range(1, 101):
-    elif percent % 10 == 1 and 11 != percent % 100:
+    if percent % 10 == 1 and 11 != percent % 100:
         print(percent, 'процент')
     elif 2 <= percent % 10 <= 4 and not 12 <= percent % 100 <= 14:
         print(percent, 'процента')
