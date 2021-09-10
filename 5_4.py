52cef96777d1e4a4b6bf21add73dad15b6de1e8e
DmitriyKardava <dmitriy.kardava@gmail.com>
2021-09-10 22:30:12 +0300

    исправил задание 4

--- 5_4.py
+++ 5_4.py
@@ -1,3 +1,9 @@
 src = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55, 66]
 dst = [curr for prev, curr in zip(src, src[1:]) if curr > prev]
 print(dst)
+
+dst = []
+for _i in range(1, len(src)):
+    if src[_i -1] < src[_i]:
+        dst.append(src[_i])
+print(dst)
