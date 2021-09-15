7242977fad6e550cb832100f4ab62393c577d387
DmitriyKardava <dmitriy.kardava@gmail.com>
2021-09-15 11:25:53 +0300

    исправил разбор параметров

--- show_sale.py
+++ show_sale.py
@@ -1,22 +1,19 @@
 from sys import argv
-_start_rec = ''
-_end_rec = ''
-if len(argv) == 2:
-    _start_rec = argv[1]
-    _end_rec = False
-elif len(argv) == 3:
-    _start_rec = argv[1]
-    _end_rec = argv[2]
+_start_rec = 1
+_end_rec = 1
+
+if len(argv) == 2 and argv[1].isdigit():
+    _start_rec = int(argv[1])
+elif len(argv) == 3 and argv[1].isdigit() and argv[2].isdigit():
+    _start_rec = int(argv[1])
+    _end_rec = int(argv[2])
 else:
     print("show_sales.py начальная запись (конечная запись)")
     quit()
-if not _start_rec.isdigit() or not _end_rec.isdigit():
-    print('Неверный номер записи')
-    quit()
-_start_rec = int(_start_rec)
-_end_rec = int(_end_rec)
+
 _start_rec = _start_rec - 1 if _start_rec > 0 else 0
 _end_rec = _end_rec - 1 if _end_rec > 0 else 0
+
 with open('bakery.csv', 'r', encoding='utf-8') as sales:
     sales.seek(_start_rec * 10)
     for _rec in sales:
